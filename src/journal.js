8b3f9a94477d8a35ac03fa8db2b19ba6cc1852e8
sgo <sgo@thinkerit.be>
2015-05-07 18:10:31 +0200

    always pass along browser details when journaling an event.

--- src/journal.js
+++ src/journal.js
@@ -1,20 +1,20 @@
 angular.module('journal', ['angular.usecase.adapter', 'rest.client', 'config', 'browser.info'])
-    .factory('journaler', ['restServiceHandler', 'usecaseAdapterFactory', 'config', JournalerFactory])
-    .run(['journaler', 'config', '$location', 'browserInfo', function(journaler, config, $location, browserInfo) {
+    .factory('journaler', ['restServiceHandler', 'usecaseAdapterFactory', 'config', 'browserInfo', '$location', JournalerFactory])
+    .run(['journaler', function(journaler) {
         window.onerror = function(msg, url, lineNumber, column, obj) {
             journaler({from:'javascript.error.reporter', payload: {
                 msg: msg,
-                stack: obj ? obj.stack : msg,
-                location: $location.absUrl(),
-                namespace: config.namespace,
-                browser: browserInfo.getInfo(),
-                userAgent: window.navigator.userAgent
+                stack: obj ? obj.stack : msg
             }});
         }
     }]);
 
-function JournalerFactory(restServiceHandler, usecaseAdapterFactory, config) {
+function JournalerFactory(restServiceHandler, usecaseAdapterFactory, config, browserInfo, $location) {
     return function(args) {
+        args.payload.namespace = config.namespace;
+        args.payload.location = $location.absUrl();
+        args.payload.browser = browserInfo.getInfo();
+        args.payload.userAgent = window.navigator.userAgent;
         var ctx = usecaseAdapterFactory({});
         ctx.params = {
             method: 'PUT',
